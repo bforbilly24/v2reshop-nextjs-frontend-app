ab8356754683f6199992cb0284ce9938530e4cd9
Halim <halimputra0701@gmail.com>
2025-06-06 21:05:19 +0700

    refactor(component): update content testiomnials section

--- src/features/about-us/components/testimonials-section.tsx
+++ src/features/about-us/components/testimonials-section.tsx
@@ -8,15 +8,15 @@ import SectionBadge from '@/components/ui/section-badge'
 const firstRow = TESTIMONIALS.slice(0, TESTIMONIALS.length / 2)
 const secondRow = TESTIMONIALS.slice(TESTIMONIALS.length / 2)
 
-const TestimonialsSection = () => {
+const TestimonialsSection: React.FC = () => {
   return (
     <div className='py-20 lg:py-32'>
-      <div className='flex flex-col items-center text-center gap-4 mb-16'>
+      <div className='flex flex-col items-center justify-center text-center mb-8 gap-y-4'>
         <AnimationContainer animation='fadeUp' delay={0.2}>
           <SectionBadge title='Testimonials' />
         </AnimationContainer>
 
-        <AnimationContainer animation='fadeUp' delay={0.3}>
+        <AnimationContainer animation='fadeUp' delay={0.4}>
           <h2 className='text-2xl md:text-4xl lg:text-5xl font-medium !leading-tight text-transparent bg-clip-text bg-gradient-to-b from-foreground to-neutral-400'>
             Loved by real estate
             <br />
@@ -24,7 +24,7 @@ const TestimonialsSection = () => {
           </h2>
         </AnimationContainer>
 
-        <AnimationContainer animation='fadeUp' delay={0.4}>
+        <AnimationContainer animation='fadeUp' delay={0.6}>
           <p className='text-sm md:text-base lg:text-lg text-muted-foreground max-w-2xl mx-auto'>
             See what our users have to say about their experience with our
             platform
@@ -32,7 +32,7 @@ const TestimonialsSection = () => {
         </AnimationContainer>
       </div>
 
-      <AnimationContainer animation='fadeUp' delay={0.5}>
+      <AnimationContainer animation='fadeUp' delay={0.8}>
         <div className='relative w-full'>
           <div className='absolute -left-1 top-0 w-20 h-full bg-gradient-to-r from-background dark:from-foreground to-transparent z-10' />
           <div className='absolute -right-1 top-0 w-20 h-full bg-gradient-to-l from-background dark:from-foreground to-transparent z-10' />
@@ -49,7 +49,7 @@ const TestimonialsSection = () => {
                 >
                   <div className='flex flex-col gap-2'>
                     <div className='flex flex-row items-center gap-2 justify-start'>
-                      <UserIcon className='w-6 h-6' />
+                      <UserIcon className='size-6' />
                       <div className='flex flex-col'>
                         <figcaption className='text-sm font-medium'>
                           {review.name}
@@ -59,25 +59,24 @@ const TestimonialsSection = () => {
                         </p>
                       </div>
                     </div>
+                    <AnimationContainer
+                      animation='fadeDown'
+                      delay={0.9 + index * 0.1}
+                      className='mt-2'
+                    >
+                      <div className='flex gap-1'>
+                        {[...Array(review.rating)].map((_, i) => (
+                          <StarIcon
+                            key={i}
+                            className='size-5 fill-yellow-400 text-yellow-400'
+                          />
+                        ))}
+                      </div>
+                    </AnimationContainer>
                     <blockquote className='mt-2 text-sm'>
                       {review.content}
                     </blockquote>
                   </div>
-
-                  <AnimationContainer
-                    animation='fadeUp'
-                    delay={0.9 + index * 0.1}
-                    className='mt-2'
-                  >
-                    <div className='flex gap-1'>
-                      {[...Array(review.rating)].map((_, i) => (
-                        <StarIcon
-                          key={i}
-                          className='w-5 h-5 fill-yellow-400 text-yellow-400'
-                        />
-                      ))}
-                    </div>
-                  </AnimationContainer>
                 </figure>
               ))}
             </Marquee>
@@ -96,7 +95,7 @@ const TestimonialsSection = () => {
                 >
                   <div className='flex flex-col gap-2'>
                     <div className='flex flex-row items-center gap-2 justify-start'>
-                      <UserIcon className='w-6 h-6' />
+                      <UserIcon className='size-6' />
                       <div className='flex flex-col'>
                         <figcaption className='text-sm font-medium'>
                           {review.name}
@@ -106,25 +105,24 @@ const TestimonialsSection = () => {
                         </p>
                       </div>
                     </div>
+                    <AnimationContainer
+                      animation='fadeDown'
+                      delay={0.9 + index * 0.1}
+                      className='mt-2'
+                    >
+                      <div className='flex gap-1'>
+                        {[...Array(review.rating)].map((_, i) => (
+                          <StarIcon
+                            key={i}
+                            className='size-5 fill-yellow-400 text-yellow-400'
+                          />
+                        ))}
+                      </div>
+                    </AnimationContainer>
                     <blockquote className='mt-2 text-sm'>
                       {review.content}
                     </blockquote>
                   </div>
-
-                  <AnimationContainer
-                    animation='fadeUp'
-                    delay={0.9 + index * 0.1}
-                    className='mt-2'
-                  >
-                    <div className='flex gap-1'>
-                      {[...Array(review.rating)].map((_, i) => (
-                        <StarIcon
-                          key={i}
-                          className='w-5 h-5 fill-yellow-400 text-yellow-400'
-                        />
-                      ))}
-                    </div>
-                  </AnimationContainer>
                 </figure>
               ))}
             </Marquee>
