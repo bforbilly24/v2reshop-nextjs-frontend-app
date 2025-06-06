31ac07677dea086d55e9b2080be17456034f27a3
Halim <halimputra0701@gmail.com>
2025-06-06 21:08:42 +0700

    feat(page): adding reproduct

---
+++ src/features/reproduct/index.tsx
@@ -0,0 +1,12 @@
+import { ReProductSection } from "./components/reproduct-section"
+
+const ReProduct = async () => {
+  return (
+    <>
+      <section id='features' className='w-full relative'>
+       <ReProductSection />
+      </section>
+    </>
+  )
+}
+export { ReProduct }
