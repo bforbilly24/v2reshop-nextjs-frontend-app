5ed696729916292ecfd8a0723f7fbefcdb5f8f85
Halim <halimputra0701@gmail.com>
2025-06-06 21:04:52 +0700

    refactor(component): update content perks section

--- src/features/about-us/components/perks-section.tsx
+++ src/features/about-us/components/perks-section.tsx
@@ -9,15 +9,15 @@ import Wrapper from '@/components/global/wrapper'
 import { Icon } from '@/components/ui/icon'
 import SectionBadge from '@/components/ui/section-badge'
 
-const PerksSection = () => {
+const PerksSection: React.FC = () => {
   return (
     <Wrapper className='py-20 lg:py-32 relative'>
-      <div className='flex flex-col items-center text-center gap-4'>
-        <AnimationContainer animation='fadeUp' delay={0.2}>
-          <SectionBadge title='Benefits' />
+      <div className='flex flex-col items-center justify-center text-center mb-8 gap-y-4'>
+        <AnimationContainer animation='fadeDown' delay={0.2}>
+          <SectionBadge title='Perks' />
         </AnimationContainer>
 
-        <AnimationContainer animation='fadeUp' delay={0.3}>
+        <AnimationContainer animation='fadeDown' delay={0.4}>
           <TypingAnimation
             duration={50}
             className='text-2xl md:text-4xl lg:text-5xl font-medium !leading-tight text-transparent bg-clip-text bg-gradient-to-b from-foreground to-neutral-400'
@@ -32,7 +32,7 @@ const PerksSection = () => {
           </TypingAnimation>
         </AnimationContainer>
 
-        <AnimationContainer animation='fadeUp' delay={0.4}>
+        <AnimationContainer animation='fadeDown' delay={0.6}>
           <p className='text-sm md:text-base lg:text-lg text-muted-foreground max-w-2xl mx-auto'>
             Connect with an eco-conscious community, access quality reuse
             materials, and be part of a circular economy movement that has real
@@ -43,10 +43,10 @@ const PerksSection = () => {
 
       <div className='relative pt-10'>
         <div className='hidden lg:block absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full z-10'>
-          <AnimationContainer animation='scaleUp' delay={0.5}>
+          <AnimationContainer animation='scaleUp' delay={0.8}>
             <Image
               src='/images/patterns/pattern-3.svg'
-              alt='Plus'
+              alt='pattern'
               width={32}
               height={32}
               className='size-full'
