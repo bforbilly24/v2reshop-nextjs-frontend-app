e4d4d7ccb4de190162589552b50aa1000f24434f
Halim <halimputra0701@gmail.com>
2025-06-10 16:48:48 +0700

    feat(page): adding shopping cart

---
+++ src/features/shopping-cart/index.tsx
@@ -0,0 +1,13 @@
+import { ShoppingCartSection } from "./components/shopping-cart-section"
+
+const ShoppingCart = async () => {
+  return (
+    <>
+      <section id='shopping-cart' className='w-full'>
+        <ShoppingCartSection />
+      </section>
+    </>
+  )
+}
+
+export { ShoppingCart }
